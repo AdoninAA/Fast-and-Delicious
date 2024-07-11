31c58e984f84cad044f5462c52f1c378b0a314a5
ArtemKorotich <korotich2004@inbox.ru>
2024-07-12 00:05:45 +0300

    forms

--- fast_and_delicious/users/admin.py
+++ fast_and_delicious/users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from cart.admin import CartTabAdmin
 
-# Register your models here.
+from users.models import User
+
+# admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["username", "first_name", "last_name", "email",]
+    search_fields = ["username", "first_name", "last_name", "email",]
+
+    
+
+    inlines = [CartTabAdmin]
