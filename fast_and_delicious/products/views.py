f8a77eb3512c49779372a15f0aa67233c56308b7
de5resnak <de5resnak@yandex.ru>
2024-07-11 13:36:12 +0300

    index view

--- fast_and_delicious/products/views.py
+++ fast_and_delicious/products/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.views import View
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'base/base.html')
