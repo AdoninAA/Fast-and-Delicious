99cc0790c7389a454b6f4a0df4e4068569d37402
de5resnak <de5resnak@yandex.ru>
2024-07-11 15:28:02 +0300

    product_detail ready

--- fast_and_delicious/products/views.py
+++ fast_and_delicious/products/views.py
@@ -5,7 +5,11 @@ from django.views.generic import DetailView
 from .models import Product, Category
 class IndexView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'base/base.html')
+        products = Product.objects.filter(is_available=True)  # Получаем доступные продукты
+        context = {
+            'products': products
+        }
+        return render(request, 'base/product_list.html', context)
 
 class ProductsDetailView(DetailView):
     model = Product
@@ -15,4 +19,4 @@ class ProductsDetailView(DetailView):
 class CategoryDetailView(DetailView):
     model = Category
     pk_url_kwarg = 'category_id'
-    template_name = 'products/category_detail.html'
+    template_name = 'nav.html'
