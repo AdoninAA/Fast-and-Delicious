35b398e786036eb487bca736e0a4367f59ab25fe
de5resnak <de5resnak@yandex.ru>
2024-07-16 17:25:42 +0300

    form fix

--- fast_and_delicious/users/forms.py
+++ fast_and_delicious/users/forms.py
@@ -9,7 +9,16 @@ class UserRegistrationForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
+from django import forms
+from .models import UserProfile
+
 class UserProfileForm(forms.ModelForm):
+    email = forms.EmailField(label='Email', required=True)
+
     class Meta:
         model = UserProfile
-        fields = ['first_name', 'last_name', 'date_of_birth', 'phone_number']
+        fields = ['first_name', 'last_name', 'email']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].initial = self.instance.user.email if self.instance.user else ''
--- fast_and_delicious/users/views.py
+++ fast_and_delicious/users/views.py
@@ -29,6 +29,7 @@ class UserRegisterView(CreateView):
 class UserLoginView(LoginView):
     template_name = 'users/login.html'
 
+
 class UserProfileView(LoginRequiredMixin, UpdateView):
     model = UserProfile
     form_class = UserProfileForm
@@ -39,6 +40,25 @@ class UserProfileView(LoginRequiredMixin, UpdateView):
         # Получаем или создаем профиль текущего пользователя
         user_profile, created = UserProfile.objects.get_or_create(user=self.request.user)
         return user_profile
+
+    def get_initial(self):
+        initial = super().get_initial()
+        user_profile = self.get_object()
+        initial['first_name'] = user_profile.user.first_name
+        initial['last_name'] = user_profile.user.last_name
+        initial['email'] = user_profile.user.email
+        return initial
+
+    def form_valid(self, form):
+        user_profile = form.save(commit=False)
+        user_profile.user.first_name = form.cleaned_data['first_name']
+        user_profile.user.last_name = form.cleaned_data['last_name']
+        user_profile.user.email = form.cleaned_data['email']
+
+        user_profile.user.save()
+        user_profile.save()
+        return super().form_valid(form)
+
 
 class UserLogoutView(LogoutView):
     next_page = reverse_lazy('users:login')
