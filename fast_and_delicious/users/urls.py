db24c7a9194df48636c0f05207c1000dd653dde6
Adonin <adoninaa0@mail.ru>
2024-07-20 23:53:54 +0300

    Remake profile

--- fast_and_delicious/users/urls.py
+++ fast_and_delicious/users/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import UserRegisterView, UserLoginView, UserProfileView, UserLogoutView
+from .views import UserRegisterView, UserLoginView, UserProfileUpdateView, UserLogoutView
 
 app_name = 'users'
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
-    path('profile/', UserProfileView.as_view(), name='profile'),
+    path('profile/', UserProfileUpdateView.as_view(), name='profile'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
 ]
--- fast_and_delicious/users/views.py
+++ fast_and_delicious/users/views.py
@@ -1,12 +1,43 @@
-from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, FormView, UpdateView
-from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth import login
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth import get_user_model
-from .models import UserProfile
-from .forms import UserRegistrationForm, UserProfileForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.edit import UpdateView, CreateView
+from django.shortcuts import redirect
+from django.contrib import messages
+from .models import User, UserProfile
+from .forms import UserUpdateForm, UserProfileUpdateForm, UserRegistrationForm
+
+class UserProfileUpdateView(LoginRequiredMixin, UpdateView):
+    model = UserProfile
+    form_class = UserProfileUpdateForm
+    template_name = 'users/profile.html'
+    success_url = reverse_lazy('users:profile')
+
+    def get_object(self, queryset=None):
+        user_profile, created = UserProfile.objects.get_or_create(user=self.request.user)
+        return user_profile
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user_form'] = UserUpdateForm(instance=self.request.user)
+        context['profile_form'] = self.get_form()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = UserProfileUpdateForm(request.POST, instance=self.get_object())
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Ваш профиль был успешно обновлен!')
+            return redirect('users:profile')
+        else:
+            messages.error(request, 'Пожалуйста, исправьте ошибки в форме.')
+            context = self.get_context_data()
+            context['user_form'] = user_form
+            context['profile_form'] = profile_form
+            return self.render_to_response(context)
 
 class UserRegisterView(CreateView):
     form_class = UserRegistrationForm
@@ -14,31 +45,14 @@ class UserRegisterView(CreateView):
     success_url = reverse_lazy('users:profile')
 
     def form_valid(self, form):
-        # Создаем пользователя
         response = super().form_valid(form)
         user = form.save()
-
-        # Создаем профиль пользователя
         UserProfile.objects.create(user=user)
-
-        # Логиним пользователя сразу после регистрации
         login(self.request, user)
-
         return response
 
 class UserLoginView(LoginView):
     template_name = 'users/login.html'
-
-class UserProfileView(LoginRequiredMixin, UpdateView):
-    model = UserProfile
-    form_class = UserProfileForm
-    template_name = 'users/profile.html'
-    success_url = reverse_lazy('products:index')
-
-    def get_object(self, queryset=None):
-        # Получаем или создаем профиль текущего пользователя
-        user_profile, created = UserProfile.objects.get_or_create(user=self.request.user)
-        return user_profile
 
 class UserLogoutView(LogoutView):
     next_page = reverse_lazy('users:login')
